5e7b4d3e61383522b9869c8276edb46ae2d80541
pav9551 <pav9551@yandex.ru>
2024-12-17 13:55:12 +0300

    added button S1 interrupt GPIO_PULLDOWN

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -57,7 +57,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin : PE10 */
   GPIO_InitStruct.Pin = GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PE13 PE14 PE15 */
